51537d8c5b7e7a078e9db45c0d87419283c7cee0
Zenalghi <114144202+Zenalghi@users.noreply.github.com>
2026-09-26 15:47:03 +0000

    Deploying to gh-pages from @ Zenalghi/r_sync_app@0d6ae12696130165ddfe3b12223c0d918351fe7e 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"af7e796e161ae0bb1ff0758c71a7105418bd9d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2604007545" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3135007522" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
